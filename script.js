909fb163eebd23902a39b777445944b461d992fa
apd5392 <adoumbia.pas@gmail.com>
2022-07-01 13:45:03 -0400

    Info Retrieved from TMDB

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-const API_KEY = ''
-const DOMAIN = 'https://api.themoviedb.org/3'
-const IMAGE_BASE_PATH = 'https://image.tmdb.org/t/p/original'
+const API_KEY = "1b95e0fa2b513c16dd02887074545dfd"; 
+const DOMAIN = "https://api.themoviedb.org/3";
+const IMAGE_BASE_PATH = "https://image.tmdb.org/t/p/original";
+const button = document.querySelector("button");
+const movieInput = document.querySelector("#search-input");
+
+
+button.addEventListener("click", async () => {
+  let movie = movieInput.value;
+  let response = await axios.get(
+    `https://api.themoviedb.org/3/search/movie?query=${movie}&api_key=${API_KEY}`
+  ).then (result => result.data.results) // arrow fucntion will return automatically. If in curly brace will need to do a return
+  console.log("Log is showing"); // Shows "Log is showing" in the console
+  console.log(response); // Shows the response
+});
+
