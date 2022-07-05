aa58157f65a297bd6123f9a40d14256ce364c078
apd5392 <adoumbia.pas@gmail.com>
2022-07-05 02:18:11 -0400

    one movie poster shows up

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-const API_KEY = "1b95e0fa2b513c16dd02887074545dfd"; 
+const API_KEY = "1b95e0fa2b513c16dd02887074545dfd";
 const DOMAIN = "https://api.themoviedb.org/3";
 const IMAGE_BASE_PATH = "https://image.tmdb.org/t/p/original";
 const button = document.querySelector("button");
 const movieInput = document.querySelector("#search-input");
-
+const list = document.querySelector(".movie-list");
 
 button.addEventListener("click", async () => {
   let movie = movieInput.value;
-  let response = await axios.get(
-    `https://api.themoviedb.org/3/search/movie?query=${movie}&api_key=${API_KEY}`
-  ).then (result => result.data.results) // arrow fucntion will return automatically. If in curly brace will need to do a return
+  let response = await axios
+    .get(
+      `https://api.themoviedb.org/3/search/movie?query=${movie}&api_key=${API_KEY}`
+    )
+    .then((result) => result.data.results); // arrow fucntion will return automatically. If in curly brace will need to do a return
   console.log("Log is showing"); // Shows "Log is showing" in the console
   console.log(response); // Shows the response
+
+  let movieList = document.createElement("div");
+  movieList.innerHTML = `<img src = ${IMAGE_BASE_PATH}${response[0].backdrop_path}>`;
+  list.append(movieList);
+  for (let i = 0; i < list.length; i++) {
+    console.log("Hi " + response);
+  }
 });
 
+list.innerText = "";
